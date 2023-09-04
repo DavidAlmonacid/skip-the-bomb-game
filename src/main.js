036f67d62cfe14b6fbf56d413429d56209f6dbe3
David Almonacid <david.almonacidf@gmail.com>
2023-09-04 12:53:48 -0500

    Clase 9

--- src/main.js
+++ src/main.js
@@ -20,6 +20,12 @@ import { emojis, maps } from './utils.js';
 
 const app = document.getElementById('app');
 const gameGrid = document.getElementById('game-grid');
+const cellPlayer = document.createElement('div');
+
+const gridWidth = gameGrid.clientWidth;
+const gridHeight = gameGrid.clientHeight;
+const cellWidth = gridWidth / 10;
+const cellHeight = gridHeight / 10;
 
 app.addEventListener('click', () => {
   gameGrid.style.userSelect = 'none';
@@ -29,12 +35,32 @@ gameGrid.addEventListener('selectstart', (event) => {
   event.preventDefault();
 });
 
+const playerPosition = {
+  X: 0,
+  Y: 0
+};
+
 const renderMap = (map) => {
   for (let i = 0; i < map.length; i++) {
     for (let j = 0; j < map[i].length; j++) {
+      const currentChar = map[i][j];
+
       const cell = document.createElement('div');
       cell.classList.add('cell');
-      cell.innerText = emojis[map[i][j]];
+      cell.innerText = emojis[currentChar];
+
+      if (currentChar === 'O') {
+        playerPosition.X = j * cellWidth;
+        playerPosition.Y = i * cellHeight;
+
+        cellPlayer.classList.add('cell');
+        cellPlayer.innerText = emojis.PLAYER;
+        cellPlayer.style.position = 'absolute';
+        cellPlayer.style.left = `${playerPosition.X}px`;
+        cellPlayer.style.top = `${playerPosition.Y}px`;
+
+        gameGrid.appendChild(cellPlayer);
+      }
 
       gameGrid.appendChild(cell);
     }
@@ -56,13 +82,55 @@ window.addEventListener('keyup', (event) => {
   };
 
   for (const key in moveKeys) {
-    if (Object.hasOwn(moveKeys, key)) {
-      const element = moveKeys[key];
+    const element = moveKeys[key];
 
-      if (element.includes(userKey)) {
-        console.log(key);
-        break;
+    if (element.includes(userKey)) {
+      let newWidth = 0;
+      let newHeight = 0;
+
+      switch (key) {
+        case 'UP':
+          newHeight = playerPosition.Y - cellHeight;
+
+          if (newHeight < 0) {
+            return;
+          }
+
+          playerPosition.Y -= cellHeight;
+          break;
+        case 'RIGHT':
+          newWidth = playerPosition.X + cellWidth;
+
+          if (newWidth > gridWidth - cellWidth) {
+            return;
+          }
+
+          playerPosition.X += cellWidth;
+          break;
+        case 'DOWN':
+          newHeight = playerPosition.Y + cellHeight;
+
+          if (newHeight > gridHeight - cellHeight) {
+            return;
+          }
+
+          playerPosition.Y += cellHeight;
+          break;
+        case 'LEFT':
+          newWidth = playerPosition.X - cellWidth;
+
+          if (newWidth < 0) {
+            return;
+          }
+
+          playerPosition.X -= cellWidth;
+          break;
       }
+
+      cellPlayer.style.left = `${playerPosition.X}px`;
+      cellPlayer.style.top = `${playerPosition.Y}px`;
+
+      break;
     }
   }
 });
